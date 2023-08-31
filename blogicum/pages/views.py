893367d799745e44b1d87e02cb42d8f536701eec
Александр Кузнецов <notjustespo@yandex.ru>
2023-08-31 23:34:06 +0300

    Скорректировать вью в pages

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 def about(request):
     template = 'pages/about.html'
-    return render(request, template, context)
+    return render(request, template)
 
 def rules(request):
     template = 'pages/rules.html'
-    return render(request, template, context)
+    return render(request, template)
